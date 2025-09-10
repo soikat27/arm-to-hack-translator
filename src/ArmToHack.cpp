db29463eada31334a62167d42207d4b0d1e2e5b1
Soikat <193371447+soikat27@users.noreply.github.com>
2025-09-10 12:42:43 -0400

    Implement translation: (MOV, ADD, SUB, RSB, CMP)
     - Add basic arithmetic translation functionality: MOV, ADD, SUB, RSB, CMP
     - Implement write_oper2() for handling numeric 2nd operands
     - Call these methods in translate() to dispatch to these methods

--- src/ArmToHack.cpp
+++ src/ArmToHack.cpp
@@ -73,32 +73,134 @@ void ArmToHack::translateFirstPass(const string &inFileName, const string &outFi
 
 void ArmToHack::translate(string &line)
 {
+    string token = peek_first(line);
 
+    if (token == "MOV")
+    {
+        translateMOV(line);
+    }
+
+    else if (token == "ADD")
+    {
+        translateADD(line);
+    }
+
+    else if (token == "SUB")
+    {
+        translateSUB(line);
+    }
+
+    else if (token == "RSB")
+    {
+        translateRSB(line);
+    }
+
+    else if (token == "CMP")
+    {
+        translateCMP(line);
+    }
 }
 
 void ArmToHack::translateMOV(string &line)
 {
+    extract_token(line);
 
+    string dest = peek_first(line);
+    string src = peek_second(line);
+
+    // write_opr2
+    write_oper2(src);
+
+    // write_opr1 and do operation
+    write_line("@" + regMap[dest]);
+    write_line("M=D");
+
+    // if dest is pc-equiv
+    write_pcjump(dest);
 }
 
 void ArmToHack::translateADD(string &line)
 {
+    extract_token(line);
 
+    string dest = extract_token(line);
+    string opr1 = peek_first(line);
+    string opr2 = peek_second(line);
+
+    // write_opr2
+    write_oper2(opr2);
+
+    // write_oper1 and do the operation
+    write_line("@" + regMap[opr1]);
+    write_line("D=M+D");
+
+    // store the sum(D) in dest
+    write_line("@" + regMap[dest]);
+    write_line("M=D");
+
+    // if dest is pc-equiv
+    write_pcjump(dest);
 }
 
 void ArmToHack::translateSUB(string &line)
 {
+    extract_token(line);
 
+    string dest = extract_token(line);
+    string opr1 = peek_first(line);
+    string opr2 = peek_second(line);
+
+    // write_opr2
+    write_oper2(opr2);
+
+    // write_oper1 and do the operation
+    write_line("@" + regMap[opr1]);
+    write_line("D=M-D");
+
+    // store result(D) in dest
+    write_line("@" + regMap[dest]);
+    write_line("M=D");
+
+    // if dest is pc-equiv
+    write_pcjump(dest);
 }
 
 void ArmToHack::translateRSB(string &line)
 {
+    extract_token(line);
 
+    string dest = extract_token(line);
+    string opr1 = peek_first(line);
+    string opr2 = peek_second(line);
+
+    // write_opr2
+    write_oper2(opr2);
+
+    // write_oper1 and do the operation
+    write_line("@" + regMap[opr1]);
+    write_line("D=D-M");
+
+    // store result(D) in dest
+    write_line("@" + regMap[dest]);
+    write_line("M=D");
+
+    // if dest is pc-equiv
+    write_pcjump(dest);
 }
 
 void ArmToHack::translateCMP(string &line)
 {
+    extract_token(line);
 
+    string opr1 = peek_first(line);
+    string opr2 = peek_second(line);
+
+    // write_opr2
+    write_oper2(opr2);
+
+    // write_oper1 and do the operation
+    write_line("@" + regMap[opr1]);
+    write_line("D=M-D");
 }
 
 void ArmToHack::translateEND(string &line)
@@ -108,7 +210,30 @@ void ArmToHack::translateEND(string &line)
 
 void ArmToHack::write_oper2(string &token)
 {
+    if (token[0] == 'R' || token == "FP" || token == "SP" || token == "LR" || token == "PC")
+    {
+        write_line("@" + regMap[token]);
+        write_line("D=M");
+    }
 
+    else if (token[0] == '#')
+    {
+        strip(token, "#+");
+
+        if (token[0] == '-')
+        {
+            strip(token, "-");
+
+            write_line("@" + token);
+            write_line("D=-A");
+        }
+
+        else
+        {
+            write_line("@" + token);
+            write_line("D=A");
+        }
+    }
 }
 
 void ArmToHack::translateJumps(string &line)
